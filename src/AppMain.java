0eb6d6f27b7dca4f4ba51b292c8349ac77535021
Yhonimard <yhoni2103@gmail.com>
2023-05-22 13:53:52 +0700

    latihan digital bootcamp 79

--- src/AppMain.java
+++ src/AppMain.java
@@ -1,14 +1,14 @@
 
 public class AppMain {
     public static void main(String[] args) throws Exception {
-        // Person person1 = new Person();
-        // person1.name = "Yhoni";
-        // person1.address = "bandung";
+        Person person1 = new Person();
+        person1.name = "Yhoni";
+        person1.address = "bandung";
 
-        // Teacher teacher1 = new Teacher();
-        // teacher1.name = "yhonimard";
-        // teacher1.address = "tanggerang";
-        // teacher1.Subject = "IPA";
+        Teacher teacher1 = new Teacher();
+        teacher1.name = "yhonimard";
+        teacher1.address = "tanggerang";
+        teacher1.subject = "IPA";
 
         Doctor doctor1 = new Doctor();
         doctor1.name = "Yohan";
@@ -23,18 +23,18 @@ public class AppMain {
         doctor2.greeting();
         System.out.println();
 
-        // Programmer programmer1 = new Programmer();
-        // programmer1.name = "Mumtaz";
-        // programmer1.address = "Ngawi";
-        // programmer1.technology = "javascript";
+        Programmer programmer1 = new Programmer();
+        programmer1.name = "Mumtaz";
+        programmer1.address = "Ngawi";
+        programmer1.technology = "javascript";
 
-        // person1.greeting();
-        // System.out.println();
-        // teacher1.greeting();
-        // System.out.println();
-        // doctor1.greeting();
-        // System.out.println();
-        // programmer1.greeting();
+        person1.greeting();
+        System.out.println();
+        teacher1.greeting();
+        System.out.println();
+        doctor1.greeting();
+        System.out.println();
+        programmer1.greeting();
 
     }
 }
